0bd75a5fa42c2d302437063fbb95d748f409d911
BKtheInfamous <134336066+BKtheInfamous@users.noreply.github.com>
2023-09-21 22:31:31 -0400

    fix styling

--- frontend/src/components/DarkThemePage.jsx
+++ frontend/src/components/DarkThemePage.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
 import Table from './Table';
-import '../index.css';
 
 function DarkThemePage() {
   const [tableData, setTableData] = useState([]);
